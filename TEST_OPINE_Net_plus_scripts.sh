d3cf93edd40cc58ea5c47b38d409b798a5aabdc6
Jian Zhang <jianzhangcs@aliyun.com>
2021-07-15 12:14:14 +0800

    Delete TEST_OPINE_Net_plus_scripts.sh

--- TEST_OPINE_Net_plus_scripts.sh
+++
@@ -1,5 +0,0 @@
-    python TEST_ISTA_Net_pp.py  --cs_ratio 10 --layer_num 20
-    python TEST_ISTA_Net_pp.py  --cs_ratio 20 --layer_num 20
-    python TEST_ISTA_Net_pp.py  --cs_ratio 30 --layer_num 20
-    python TEST_ISTA_Net_pp.py  --cs_ratio 40 --layer_num 20
-    python TEST_ISTA_Net_pp.py  --cs_ratio 50 --layer_num 20
